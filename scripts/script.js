a282902e403b1d334e694dfccbc1c0a5d5e0ee5c
mypaxpax <thomasgade@gmail.com>
2023-01-04 19:22:47 +0100

    Bit of cleanup

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ const form = document.querySelector("#form");
 const bookListing = document.querySelector(".book-listing");
 
 // The action taken when the form is submited - calls the function to add a new book
-form.addEventListener("submit", addBookTomyLib);
+form.addEventListener("submit", addBookToLib);
 
 // The array tot store the book objects
 let myLib = [
@@ -40,7 +40,7 @@ function Books(title, author, pages, readStatus) {
 }
 
 //function that is being called when the form is submited
-function addBookTomyLib() {
+function addBookToLib() {
   event.preventDefault();
   //creates a new book object
   const newBook = new Books(title.value, author.value, pages.value, read.value);
@@ -58,21 +58,23 @@ function loopBooks() {
     const removeBtn = document.createElement("div");
     const bookTitle = document.createElement("p");
     const changeRead = document.createElement("div");
+    changeRead.classList.add("change-read-status");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookRead = document.createElement("p");
+
     removeBtn.innerHTML = `<button class="removebtn" onclick="removeBook(${i})">Delete</button>`;
-    changeRead.innerHTML = `<button class="removebtn" onclick="toggleRead(${i})">Change status</button>`;
+    changeRead.innerHTML = `<button onclick="toggleRead(${i})">Change Status</button>`;
 
     bookTitle.className = "book-title-listing";
     bookTitle.textContent = myLib[i].title;
 
-    const bookAuthor = document.createElement("p");
     bookAuthor.className = "book-author-listing";
     bookAuthor.textContent = myLib[i].author;
 
-    const bookPages = document.createElement("p");
     bookPages.className = "book-pages-listing";
     bookPages.textContent = myLib[i].pages;
 
-    const bookRead = document.createElement("p");
     bookRead.className = "book-read-listing";
     bookRead.textContent = myLib[i].read;
 
@@ -100,4 +102,5 @@ function toggleRead(i) {
   }
   loopBooks();
 }
+
 loopBooks();
