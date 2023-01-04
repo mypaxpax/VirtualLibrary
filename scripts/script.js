2d2a343183f70bce0e8b646725ba146d935c261b
mypaxpax <thomasgade@gmail.com>
2023-01-04 18:26:21 +0100

    Read/Not read button

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,12 @@
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
-const isRead = document.querySelector("#isRead");
+const read = document.querySelector("#read");
 const form = document.querySelector("#form");
 const bookListing = document.querySelector(".book-listing");
 
 // The action taken when the form is submited - calls the function to add a new book
-form.addEventListener("submit", addBookToLibrary);
+form.addEventListener("submit", addBookTomyLib);
 
 // The array tot store the book objects
 let myLib = [
@@ -15,40 +15,35 @@ let myLib = [
     title: "Dark Matter",
     author: "Michelle Paver",
     pages: 246,
-    isRead: "No",
+    read: "Read",
   },
   {
     title: "Imaginary Friend",
     author: "Stephen Chbosky",
     pages: 705,
-    isRead: "Yes!",
+    read: "Not read",
   },
   {
     title: "Stolen Tongues",
     author: "Felix Blackwell",
     pages: 320,
-    isRead: "Yes!",
+    read: "Read",
   },
 ];
 
 //Object that creates the book
-function Books(title, author, pages, isRead) {
+function Books(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = isRead;
+  this.read = readStatus;
 }
 
 //function that is being called when the form is submited
-function addBookToLibrary() {
+function addBookTomyLib() {
   event.preventDefault();
   //creates a new book object
-  const newBook = new Books(
-    title.value,
-    author.value,
-    pages.value,
-    isRead.value
-  );
+  const newBook = new Books(title.value, author.value, pages.value, read.value);
   //adds the newly created book to the array
   myLib.push(newBook);
   loopBooks();
@@ -62,27 +57,33 @@ function loopBooks() {
   for (let i = 0; i < myLib.length; i++) {
     const removeBtn = document.createElement("div");
     const bookTitle = document.createElement("p");
+    const changeRead = document.createElement("div");
+    removeBtn.innerHTML = `<button class="removebtn" onclick="removeBook(${i})">Delete</button>`;
+    changeRead.innerHTML = `<button class="removebtn" onclick="toggleRead(${i})">Change status</button>`;
 
     bookTitle.className = "book-title-listing";
     bookTitle.textContent = myLib[i].title;
-    removeBtn.innerHTML = `<button class="removebtn" onclick="removeBook(${i})">Delete</button>`;
 
     const bookAuthor = document.createElement("p");
     bookAuthor.className = "book-author-listing";
     bookAuthor.textContent = myLib[i].author;
-    removeBtn.innerHTML = `<button class="removebtn" onclick="removeBook(${i})">Delete</button>`;
 
     const bookPages = document.createElement("p");
     bookPages.className = "book-pages-listing";
     bookPages.textContent = myLib[i].pages;
-    removeBtn.innerHTML = `<button class="removebtn" onclick="removeBook(${i})">Delete</button>`;
 
     const bookRead = document.createElement("p");
     bookRead.className = "book-read-listing";
-    bookRead.textContent = myLib[i].isRead;
-    removeBtn.innerHTML = `<button class="removebtn" onclick="removeBook(${i})">Delete</button>`;
+    bookRead.textContent = myLib[i].read;
 
-    bookListing.append(bookTitle, bookAuthor, bookPages, bookRead, removeBtn);
+    bookListing.append(
+      bookTitle,
+      bookAuthor,
+      bookPages,
+      bookRead,
+      removeBtn,
+      changeRead
+    );
   }
 }
 
@@ -91,4 +92,12 @@ function removeBook(i) {
   loopBooks();
 }
 
+function toggleRead(i) {
+  if (myLib[i].read === "Read") {
+    myLib[i].read = "Not read";
+  } else {
+    myLib[i].read = "Read";
+  }
+  loopBooks();
+}
 loopBooks();
